b7074f690d5f989873b6b3a607b980caae999f6d
Mattis Jeppsson <mattisj@ed-5352-03.studat.chalmers.se>
2010-03-11 15:46:03 +0100

    set up basic API of typechecker

--- haskell.typechecker.js
+++ haskell.typechecker.js
@@ -1,3 +1,33 @@
 (function (typechecker, ast) {
-     alert ("apa");
+     var astt = new ast.Module([
+			       new ast.Variable(new ast.VariableBinding("inc"),
+						new ast.Lambda(new ast.VariableBinding("x"),
+							       new ast.Application(new ast.Application(new ast.VariableLookup("+"),
+												       new ast.VariableLookup("x")),
+										   new ast.Constant(new ast.Num(1))))),
+			       new ast.Variable(new ast.VariableBinding("inc2"),
+						new ast.Lambda(new ast.VariableBinding("x"),
+							       new ast.Application(new ast.VariableLookup("inc"),
+										   new ast.Application(new ast.VariableLookup("inc"),
+												       new ast.VariableLookup("x"))))),
+			       new ast.Variable(new ast.VariableBinding("main"),
+						new ast.Application(new ast.VariableLookup("alert"),
+								    new ast.Application(new ast.VariableLookup("inc2"),
+											new ast.Constant(new ast.Num(2)))))
+			   ]);
+     var asttt = new ast.Application(new ast.Application(new ast.VariableLookup("+"),
+							 new ast.VariableLookup("x")),
+				     new ast.Constant(new ast.Num(1))); // ((x + :: (Num -> Num)) 1 :: Num)
+
+     typechecker.test = function() {
+	 alert(typechecker.typecheck(astt));
+     }
+
+     typechecker.typecheck = function(astt) {
+	 return typechecker.infer(asttt);
+     }
+
+     typechecker.infer = function(astt) {
+	 return astt.type;
+     }
 }) (haskell.typechecker, haskell.ast);
