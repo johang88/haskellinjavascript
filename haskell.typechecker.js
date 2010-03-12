4233b3882793c252a9757de30c8ccbd6cbc129dc
Mattis Jeppsson <mattisj@ed-3358-11.studat.chalmers.se>
2010-03-12 15:52:41 +0100

    added basic definitions for type checker primitives

--- haskell.typechecker.js
+++ haskell.typechecker.js
@@ -18,16 +18,87 @@
      var asttt = new ast.Application(new ast.Application(new ast.VariableLookup("+"),
 							 new ast.VariableLookup("x")),
 				     new ast.Constant(new ast.Num(1))); // ((x + :: (Num -> Num)) 1 :: Num)
+     // asttt = new ast.Constant(new ast.Num(1));
+
+     ast.Application.prototype.infer = function() {
+	 return undefined;
+     };
+
+     ast.Application.prototype.check = function(type) {
+	 return this.infer() == type;
+     };
+
+     ast.Constant.prototype.check = function(type) {
+	 return this.infer() == type;
+     };
+
+     ast.Constant.prototype.infer = function() {
+	 if(this.hsType == undefined) {
+	     this.hsType = this.value.infer();
+	 }
+	 return this.hsType;
+     };
+
+     ast.Num.prototype.infer = function() {
+	 this.hsType = "Integer";
+	 return "Integer";
+     };
+
+     ast.Num.prototype.check = function(type) {
+	 return this.infer() == type;
+     };
+
+     ast.Variable.prototype.infer = function() {
+	 if(this.hsType == undefined) {
+	     this.hsType = this.expression.infer();
+	 }
+	 return this.hsType;
+     };
+
+     typechecker.Star = function() {
+     };
+     typechecker.Kfun = function(kind1, kind2) {
+	 this.kind1 = kind1;
+	 this.kind2 = kind2;
+     };
+
+     typechecker.Tyvar = function(id, kind) {
+	this.kind = function() { return kind; }; 
+     };
+
+     typechecker.Tycon = function(id, kind) {
+	this.kind = function() { return kind; };
+     };
+
+     typechecker.TVar = function(tyvar) {
+	 this.kind = function() { return tyvar.kind(); };
+	 this.apply = function(subst) {
+	     if (subst[tyvar] != undefined) {
+		 return subst[tyvar];
+	     }
+	     return (new typechecker.TVar(tyvar));
+	 };
+	 this.tv = function() { return [tyvar]; };
+     };
+     typechecker.TCon = function(tycon) {
+	 this.kind = function() { return tycon.kind(); };
+	 this.apply = function(subst) { return this; };
+	 this.tv = function() { return []; };
+     };
+     typechecker.TAp = function(t1, t2) {
+	 this.kind = function() { return t1.kind().kind2;  };
+	 this.apply = function(subst) { return new typechecker.TAp(t1.apply(),t2.apply()); };
+	 this.tv = function() { };
+     };
+     typechecker.TGen = function() {
+	 // this.kind = function() { }; - should probably throw an exception
+	 this.apply = function(subst) { return this; };
+	 this.tv = function() { return []; };
+     };
 
      typechecker.test = function() {
-	 alert(typechecker.typecheck(astt));
-     }
+	 alert(asttt.infer());
+	 alert(asttt.check("Integer"));
+     };
 
-     typechecker.typecheck = function(astt) {
-	 return typechecker.infer(asttt);
-     }
-
-     typechecker.infer = function(astt) {
-	 return astt.type;
-     }
 }) (haskell.typechecker, haskell.ast);
