65c8ae483db7fa046ffcaf7261f290999e58c695
Johan Gustafsson <johan@johan-laptop.(none)>
2010-03-14 14:38:08 +0100

    Prelude is now correct

--- Prelude.hs
+++ Prelude.hs
@@ -8,10 +8,7 @@ module Prelude where {
     ($) f x = f x;
     (.) f g = \x -> f $ g x;
     
-    map f [] = [];
-    map f (x:xs) = f x : map f xs;
-    
     id x = x;
     
-    fix f = let x = f x in x
+    fix f = let { x = f x; } in x;
 }
