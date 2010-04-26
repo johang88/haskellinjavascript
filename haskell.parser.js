22e49b0d4c6933e5cfa8584ee778c286c28ab0e7
Johan Gustafsson <johan@johan-laptop.(none)>
2010-04-26 14:44:32 +0200

    Added special character stuff to lexer

--- haskell.parser.js
+++ haskell.parser.js
@@ -907,8 +907,8 @@ Todo:
             this.indent = indent;
         }
         
-        var lexeme = join_action(repeat1(negate(choice('\n', '\r', ' ', '\t', ';', '{', '}', '(', ')'))), "");
-        lexeme = choice(';', '{', '}', join_action(sequence('(', sym, ')'), ""), '(', ')', lexeme);
+        var lexeme = join_action(repeat1(negate(choice('\n', '\r', ' ', '\t', ';', '{', '}', '(', ')', '[', ']', ','))), "");
+        lexeme = choice(';', '{', '}', join_action(sequence('(', sym, ')'), ""), '(', ')', '[', ']', ',', lexeme);
         lexeme = action(lexeme, function(ast) {
             var indent = lexer_state.current;
             lexer_state.current += ast.length;
