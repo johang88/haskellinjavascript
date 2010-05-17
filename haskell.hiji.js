ffa3a01d748a69c74f080ec51c5d336e3395d448
Johan Gustafsson <johan@johan-laptop.(none)>
2010-05-17 16:15:13 +0200

    show hack and pretty printing for lists

--- haskell.hiji.js
+++ haskell.hiji.js
@@ -98,11 +98,52 @@ commands[":help"] = "HELP";
                     try {
                         var newLine = makeEntered(modules, line);
                         
-                        var result = evaluateHaskell(line, env);
-                        if (result.force) {
-                            result = result.force();
-                        } else if (result.ptrs) {
-                            result = result.ptrs[0].thunk.force();
+                        var showResult = function(result) {
+                            if (result.type == "Data") {
+                                var str = result.identifier;
+                                var op = " ";
+                                
+                                if (str == "I#") {
+                                    str = "";
+                                } else if (str == ":") {
+                                    str = "";
+                                    op = ",";
+                                }
+                                
+                                if (result.ptrs) {
+                                    var first = true;
+                                    for (var i = 0; i < result.ptrs.length; i++) {
+                                        if (str.length == 0 && first) {
+                                            str = showResult(result.ptrs[i].dereference());
+                                            if (str.str) 
+                                                str = str.str;
+                                            first = false;
+                                        } else {
+                                            var res = showResult(result.ptrs[i].dereference());
+                                            if (res.str)
+                                                res = res.str;
+                                            str = str + op + res;
+                                        }
+                                    }
+                                }
+                                
+                                return { str: str, isList: op == "," };
+                            } if (result.force) {
+                                return result.force();
+                            } else if (result.ptrs) {
+                                return result.ptrs[0].dereference();
+                            } else {
+                                return result; 
+                            }
+                        }
+                        
+                        var result = showResult(evaluateHaskell(line, env));
+                        if (result.isList) {
+                            result = result.str;
+                            result = result.substring(0, result.length - 3);
+                            result = "[" + result + "]";
+                        } else if (result.str) {
+                            result = result.str;
                         }
                         
                         var output = makeOutput(result);
