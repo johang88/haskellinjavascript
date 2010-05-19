ea4732da16bab9da0e0c17b58949096f076343b8
Mattis Jeppsson <mattis@mattis-laptop.(none)>
2010-05-19 15:01:17 +0200

    merging

--- haskell.js
+++ haskell.js
@@ -1,42 +1,7 @@
 var haskell = {
-	parser: {},
-	interpreter: {},
-	ast: {},
-	typechecker: {}
-};
-
-haskell.env = function() {
-	this.symbols = {};
-	this.arg_list = {};
-	
-	this.set_symbol = function(symbol, value, evaluated) {
-		var s = {};
-		s.value = value;
-		s.evaluated = evaluated == undefined ? false : evaluated;
-		
-		this.symbols[symbol] = s;
-	}
-	
-	this.get_symbol = function(symbol) {
-		return this.symbols[symbol];
-	}
-	
-	this.exists_symbol = function(symbol) {
-		return this.symbols[symbol] != undefined;
-	}
-	
-	this.set_arg_list = function(args) {
-		this.arg_list = args;
-	}
-}
-
-haskell.eval = function(code) {
-	if (typeof(console) == "undefined") {
-		console = {};
-		console.log = function() {};
-	}
-	
-	var ast = haskell.parser.parse(code).ast;
-	console.log("%o", ast);
-	return haskell.interpreter.interpret(new haskell.env(), ast);
+    parser: {},
+    interpreter: {},
+    ast: {},
+    primitives: {},
+    utilities: {}
 };
